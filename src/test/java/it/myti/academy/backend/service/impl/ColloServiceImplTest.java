ad9ae24cbd887b6c9b42cb420bf30b19c6238d47
David Bonomels <d.bonomelli@myti.it>
2019-02-20 15:52:56 +0100

    Moved packages

---
+++ src/test/java/it/myti/academy/backend/service/impl/ColloServiceImplTest.java
@@ -0,0 +1,21 @@
+package it.myti.academy.backend.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by david at 2019-02-20
+ */
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class ColloServiceImplTest {
+
+    @Test
+    public void getSpedizioniAttiveByUtente() {
+
+    }
+}
