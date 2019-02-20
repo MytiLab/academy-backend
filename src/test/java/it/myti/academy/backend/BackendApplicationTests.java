b5b1a2b83b98f7547a1dfadb8d9bc71e7d8f880b
David Bonomels <d.bonomelli@myti.it>
2019-02-20 23:13:31 +0100

    Added several tests

--- src/test/java/it/myti/academy/backend/BackendApplicationTests.java
+++ src/test/java/it/myti/academy/backend/BackendApplicationTests.java
@@ -7,13 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest()
-public class BackendApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
+//@RunWith(SpringRunner.class)
+//@SpringBootTest()
+//public class BackendApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
 
